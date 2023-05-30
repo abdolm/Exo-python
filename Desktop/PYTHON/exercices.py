0bd1fd1fa8d182aaafad93f1ca3c16cf9751a62e
simplon student <simplonetudiant59@gmail.com>
2023-05-30 13:07:05 +0200

    exo python

---
+++ Desktop/PYTHON/exercices.py
@@ -0,0 +1,75 @@
+# import turtle
+
+# Ceci est un commentaire, les commentaires sont ignorés par l'ordinateur.
+# Ils sont ici pour permettre aux développeur de garder leur code compréhensible 
+# (pour eux même ou pour les autres)
+
+# turtle.speed(8) # permet de régler la vitesse d'animation de la tortue (1 étant le plus lent, 10 le plus rapide)
+
+# Début des instructions permettant de dessiner : insérer votre code ici !
+
+# turtle.forward(100) # Ici une instruction permettant d'avancer de 200px
+# turtle.left(90) # Ici une instruction permettant de dessiner un cercle de rayon 30px
+# turtle.forward(100) # Ici une instruction permettant d'avancer de 200px
+# turtle.right(90)
+# turtle.forward(-100) # Ici une instruction permettant d'avancer de 200px
+# turtle.left(90) # Ici une instruction permettant de dessiner un cercle de rayon 30px
+# turtle.forward(-100) # Ici une instruction permettant d'avancer de 200px
+
+# fin des instruction permettant de dessiner.
+
+# turtle.done() # permet d'indiquer à la tortue que nous avons fini le dessin
+
+
+# turtle.speed(8)
+
+# turtle.forward(100) # Ici une instruction permettant d'avancer de 200px
+# turtle.left(72) # Ici une instruction permettant de dessiner un cercle de rayon 30px
+# turtle.forward(100) # Ici une instruction permettant d'avancer de 200px
+# turtle.left(72)
+# turtle.forward(100) # Ici une instruction permettant d'avancer de 200px
+# turtle.forward(100)
+# turtle.left(72)
+# turtle.forward(100)
+
+
+
+# turtle.done()
+
+# turtle.speed(8)
+
+# turtle.forward(100) # Ici une instruction permettant d'avancer de 200px
+# turtle.left(90) # Ici une instruction permettant de dessiner un cercle de rayon 30px
+# turtle.forward(100) # Ici une instruction permettant d'avancer de 200px
+# turtle.left(135)
+# turtle.forward(140) # Ici une instruction permettant d'avancer de 200px
+# turtle.goto(100,0)
+# turtle.left(135)
+# turtle.forward(100) 
+# turtle.left(-45)
+# turtle.forward(-140) # Ici une instruction permettant d'avancer de 200px
+# turtle.goto(100,-10)
+
+# turtle.forward(100)
+
+
+
+# turtle.done()
+
+
+
+# ans = input("Type the miles disatnce you want to convert in km: ")
+# miles = int(ans)
+# km = miles * 1.6
+# print(f"{miles} miles est équivalent à {km} km")
+
+number = int(input("Entrez un nombre "))
+
+if(number % 2 != 0):
+    # code qui s'exécute si nbUtilisateur est plus petit que 0
+    print("Votre nombre est IMPAIRE")
+else:
+    # sinon, c'est ce code qui s'exécute
+    print("Votre nombre est PAIRE !")
+
+
